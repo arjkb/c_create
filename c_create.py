7325a244ea0b95fea0a681073283277c1b724ab5
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2016-07-26 21:35:05 +0530

    creating filename.c hello world program

--- c_create.py
+++ c_create.py
@@ -1,8 +1,12 @@
 import argparse
 
+from shutil import copyfile
+
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help="name of C file to be created")
 args = parser.parse_args()
 
-c_filename = args.filename
+c_filename = args.filename + ".c"
 print(" Filename: {}".format(c_filename))
+
+copyfile("hello_world.c", c_filename)
