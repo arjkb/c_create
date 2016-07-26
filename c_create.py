52ad46c61885be62417f736ae5fe4dd7af368886
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2016-07-26 21:29:23 +0530

    storing filename in a separate variable

--- c_create.py
+++ c_create.py
@@ -3,4 +3,6 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help="name of C file to be created")
 args = parser.parse_args()
-print(" Filename: {}".format(args.filename))
+
+c_filename = args.filename
+print(" Filename: {}".format(c_filename))
